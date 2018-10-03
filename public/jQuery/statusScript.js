61dd246526a45ac35a6d75843e1392757a60cc69
Sathira Kittisukmongkol <nong.jamie.jamie@gmail.com>
2018-10-03 16:59:47 +0700

    Practise checking order status

--- public/jQuery/statusScript.js
+++ public/jQuery/statusScript.js
@@ -1,11 +1,33 @@
 $( () => {
 
+  let array = [
+    {
+      orderNumber: 000001,
+      number: 1,
+      productName: "Kong",
+      amount: 2,
+      cost: 3000,
+      status: "sending"
+    }
+  ];
+
   $('#status_inputform').on('keypress', (event) => {
     if(event.keyCode == 13) {
-      let x = $('#status_inputform').val();
-      console.log( x );
-      console.log('from inside if');
+      let input = $('#status_inputform').val();
+      if( containInArray( input ) ) {
+
+      }
     }
   } );
+
+  function containInArray( input ) {
+    for( i = 0 ; i < array.length ; i++ ) {
+      if( array[i].orderNumber == input ) {
+        console.log("have");
+        return true;
+      }
+    }
+    return false;
+  }
 
 } );
