236419a378d1d833d63f898c1953c3944ea33a75
Sathira Kittisukmongkol <nong.jamie.jamie@gmail.com>
2018-10-03 11:44:29 +0700

    Try js in status page(success)

--- public/jQuery/statusScript.js
+++ public/jQuery/statusScript.js
@@ -1,9 +1,10 @@
 $( () => {
 
-  $('#status-inputform').on('keypress', (event) => {
-    if(event.keyCode === 13) {
-      let x = $('#status-inputform').val();
+  $('#status_inputform').on('keypress', (event) => {
+    if(event.keyCode == 13) {
+      let x = $('#status_inputform').val();
       console.log( x );
+      console.log('from inside if');
     }
   } );
 
