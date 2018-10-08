149d21e66161972e2ed9de63cf357326d355c6f6
Pittayut Sothanakul <pittayut.sot@ku.th>
2018-10-08 17:35:36 +0700

    can change price

--- public/script/selectPromotion.js
+++ public/script/selectPromotion.js
@@ -4,12 +4,17 @@
 //   myimage.src ="https://cdn.pixabay.com/photo/2017/04/26/18/36/cartoon-art-2263376_960_720.png";
 // }
 
-function jsDropDown(imgid,newing){
-  console.log("js is running");
+function jsDropDown(imgid,nameid,txtid,newing){
   document.getElementById(imgid).src = "../pictures/" + newing + ".jpg"
+  if(newing == "Logo_Red"){
+    document.getElementById(txtid).innerHTML = "฿ 999";
+    document.getElementById(nameid).innerHTML = "Promotion RedLabel with 9 Mixer";
+  } else if(newing == "Logo_Black"){
+    document.getElementById(txtid).innerHTML = "฿ 1399";
+    document.getElementById(nameid).innerHTML = "Promotion BlackLabel with 9 Mixer";
+  }
+  // console.log("newing = "+newing);
+  // console.log("txt = "+txtPromotion);
+  // document.getElementById(txtPromotion).innerHTML = "Promotion RedLabel with 9 Mixer";
+
 }
-// function setImageSrc(){
-//    cID = document.forms[0]
-//    var x = document.images
-//    x[0].src = "../pictures/" +cID+".jpg";
-// }
