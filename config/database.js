899eb64536adc8c55b53b63910f8e668e66b8796
wasuthun wanaphongthipakorn <pond_Zen@hotmail.com>
2018-10-14 16:46:14 +0700

    jp

--- config/database.js
+++ config/database.js
@@ -4,5 +4,6 @@ module.exports = {
     getList : URL + 'getAccountList',
     addAccount : URL + 'addAccount',
     login : URL + 'login',
-    getAccountByUsername : URL + 'getAccountByUsername'
+    getAccountByUsername : URL + 'getAccountByUsername',
+    getCategory: URL+'getCategory'
 }
