2e1a19f4164547a4d14cd85fe9c7e26d49645c6f
Sathira Kittisukmongkol <nong.jamie.jamie@gmail.com>
2018-11-05 23:15:42 +0700

    Initialize commit

--- public/script/summary.js
+++ public/script/summary.js
@@ -142,12 +142,6 @@ $( () => {
     <p id="noticeDetail">If all details are correct, Click the confrim button below to complete the transaction.</p>
   `);
 
-  // Cofirm button clicked.
-  $('#yesButton').click( () => {
-    console.log('Go to summary page.');
-    window.location = "http://localhost:3000/confirmTransaction";
-  });
-
   // Cancel button clicked then go to whisky page.
   $('#cancelButton').click( () => {
     console.log('Cancel the payment, go back to payment page.');
