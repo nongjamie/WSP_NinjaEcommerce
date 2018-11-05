7407b3420dd099b426aa14619cec0e068545fbcf
Sathira Kittisukmongkol <nong.jamie.jamie@gmail.com>
2018-11-05 16:37:29 +0700

    Finish payment page

--- public/script/appointment.js
+++ public/script/appointment.js
@@ -1,22 +1,36 @@
 $(function () {
-    $('#confirm').on('click', function(e){
-        const username = $('#userNavBarUsername').text()
-        $.ajax({
-            url: '/myappointment/'+username,
-            data:{
-                'username': username,
-            },
-            type: 'POST',
-            success: function(data) {
-                if(data.return_code === '400'){
-                    console.log('error')
-                }else{
-                    console.log(data)
-                }
-            },
-            error: function(error){
-                console.log('error check out')
-            }
-        })
-    })
+
+  // Cofirm button clicked.
+  $('#yesButton').click( () => {
+    console.log('Go to payment page.');
+    window.location = "http://localhost:3000/payment";
+  });
+
+  // Cancel button clicked then go to whisky page.
+  $('#cancelButton').click( () => {
+    console.log('Cancel the confirm order, go back to whisky page.');
+    window.location = "http://localhost:3000/mycart/None";
+  });
+
+  $('#confirm').on('click', function(e){
+      const username = $('#userNavBarUsername').text()
+      $.ajax({
+          url: '/myappointment/'+username,
+          data:{
+              'username': username,
+          },
+          type: 'POST',
+          success: function(data) {
+              if(data.return_code === '400'){
+                  console.log('error')
+              }else{
+                  console.log(data)
+              }
+          },
+          error: function(error){
+              console.log('error check out')
+          }
+      })
+  });
+
 })
