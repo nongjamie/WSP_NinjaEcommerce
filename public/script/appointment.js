42f2c23acb26ef44a10970fdaa2c175802700864
Sathira Kittisukmongkol <nong.jamie.jamie@gmail.com>
2018-11-06 00:06:34 +0700

    Paul help

--- public/script/appointment.js
+++ public/script/appointment.js
@@ -10,7 +10,8 @@ $(function () {
   // Cancel button clicked then go to whisky page.
   $('#cancelButton').click( () => {
     console.log('Cancel the appointment, go back to my cart page.');
-    window.location = "http://localhost:3000/mycart/None";
+    const username = $('#userNavBarUsername').text()
+    window.location = "http://localhost:3000/mycart/"+username;
   });
 
   $('#confirm').on('click', function(e){
