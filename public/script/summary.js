3ba70d01eed478abdf6683c6f69c5b82cb229e81
Sathira Kittisukmongkol <nong.jamie.jamie@gmail.com>
2018-11-16 17:29:03 +0700

    Remove duplicate code

--- public/script/summary.js
+++ public/script/summary.js
@@ -1,139 +1,7 @@
 $( () => {
 
-
-
-  let styleHeadColumnCart = "style=\"height: 50px; width: 100%;\"";
-
-  // Set the head of row
-  $('.pictureColumn').append(`
-    <div ${styleHeadColumnCart}>
-      <p id="productSenc" style="font-weight:bold;">
-        Product
-      </p>
-    </div>
-    `);
-  $('.productNameColumn').append(`
-    <div ${styleHeadColumnCart}>
-      <p id="nameSenc" style="font-weight:bold;">
-        Name
-      </p>
-    </div>
-    `);
-  $('.productQuantityColumn').append(`
-    <div ${styleHeadColumnCart}>
-      <p id="quantitySenc" style="font-weight:bold;">
-        Quantity
-      </p>
-    </div>
-    `);
-  $('.productUnitPriceColumn').append(`
-    <div ${styleHeadColumnCart}>
-      <p id="unitPriceSenc" style="font-weight:bold;">
-        UnitPrice
-      </p>
-    </div>
-    `);
-  $('.productTotalPriceColumn').append(`
-    <div ${styleHeadColumnCart}>
-      <p id="totalCostSenc" style="font-weight:bold;">
-        TotalCost
-      </p>
-    </div>
-    `);
-
-
-  $('.summaryPersonal').html(`
-    <p>Personal</p>
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="basic-addon1">Name</span>
-        <input type="number" class="form-control" placeholder="${exampleCreditcard.cardHolderName}" aria-label="Username" aria-describedby="basic-addon1" disabled>
-      </div>
-    </div>
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="basic-addon1">Surname</span>
-        <input type="number" class="form-control" placeholder="${exampleCreditcard.creditCardNumber}" aria-label="Username" aria-describedby="basic-addon1" disabled>
-      </div>
-    </div>
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="basic-addon1">Birthday</span>
-        <input type="number" class="form-control" placeholder="${exampleCreditcard.expirationDate}" aria-label="Username" aria-describedby="basic-addon1" disabled>
-      </div>
-    </div>
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="basic-addon1">Sex</span>
-        <input type="number" class="form-control" placeholder="${exampleCreditcard.cardSecurityCode}" aria-label="Username" aria-describedby="basic-addon1" disabled>
-      </div>
-    </div>
-    </br>
-  `);
-
-  // Set the detail of appointment
-  $('.summaryAppointment').html(`
-    <p>Address</p>
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="basic-addon1">Address</span>
-        <input type="number" class="form-control" placeholder="${exampleAppointment.address}" aria-label="Username" aria-describedby="basic-addon1" disabled>
-      </div>
-    </div>
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="basic-addon1">Distrinct</span>
-        <input type="number" class="form-control" placeholder="${exampleAppointment.distrinct}" aria-label="Username" aria-describedby="basic-addon1" disabled>
-      </div>
-    </div>
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="basic-addon1">Province</span>
-        <input type="number" class="form-control" placeholder="${exampleAppointment.province}" aria-label="Username" aria-describedby="basic-addon1" disabled>
-      </div>
-    </div>
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="basic-addon1">ZipCode</span>
-        <input type="number" class="form-control" placeholder="${exampleAppointment.zipCode}" aria-label="Username" aria-describedby="basic-addon1" disabled>
-      </div>
-    </div>
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="basic-addon1">Telephone</span>
-        <input type="number" class="form-control" placeholder="${exampleAppointment.telephone}" aria-label="Username" aria-describedby="basic-addon1" disabled>
-      </div>
-    </div>
-  `);
-
-  $('.summaryPayment').html(`
-    <p>Payment</p>
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="basic-addon1">Card holder's name</span>
-        <input type="number" class="form-control" placeholder="${exampleCreditcard.cardHolderName}" aria-label="Username" aria-describedby="basic-addon1" disabled>
-      </div>
-    </div>
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="basic-addon1">Credit card number</span>
-        <input type="number" class="form-control" placeholder="${exampleCreditcard.creditCardNumber}" aria-label="Username" aria-describedby="basic-addon1" disabled>
-      </div>
-    </div>
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="basic-addon1">Expiration date</span>
-        <input type="number" class="form-control" placeholder="${exampleCreditcard.expirationDate}" aria-label="Username" aria-describedby="basic-addon1" disabled>
-      </div>
-    </div>
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="basic-addon1">Card security code</span>
-        <input type="number" class="form-control" placeholder="${exampleCreditcard.cardSecurityCode}" aria-label="Username" aria-describedby="basic-addon1" disabled>
-      </div>
-    </div>
-    </br>
-  `);
+  // Initial setup in cart, personal, address, payment section.
+  initialSetUp();
 
   // Cofirm button clicked.
   $('#yesButton').click( () => {
@@ -172,3 +40,116 @@ $( () => {
   // });
 
 } );
+
+function initialSetUp() {
+
+  // Table head row initial setup
+
+  let headOfRow = [
+    {
+      id: "\'productSenc\'",
+      class: "\'.pictureColumn\'",
+      detail: "Product"
+    },
+    {
+      id: "\'nameSenc\'",
+      class: "\'.productNameColumn\'",
+      detail: "Name"
+    },
+    {
+      id: "\'quantitySenc\'",
+      class: "\'.productQuantityColumn\'",
+      detail: "Quality"
+    },
+    {
+      id: "\'unitPriceSenc\'",
+      class: "\'.productUnitPriceColumn\'",
+      detail: "UnitPrice"
+    },
+    {
+      id: "\'totalCostSenc\'",
+      class: "\'.productTotalPriceColumn\'",
+      detail: "TotalCost"
+    }
+  ];
+  for(let j = 0 ; j <  ; j++ ) {
+    $(headOfRow[j].class).append(`
+      <div style="height: 50px; width: 100%;">
+        <p id=${headOfRow[j].id} style="font-weight:bold;">
+          ${headOfRow[j].detail}
+        </p>
+      </div>
+    `);
+  }
+
+  // Table body initial setup.
+
+  // Personal initial setup
+
+  let personal = [ "Name", "Surname", "Birthday", "Sex" ];
+  let personalCode = '<p>Personal</p>';
+  for(let i = 0 ; i < personal.length ; i++) {
+    personalCode = personalCode + `
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon1">${personal[i]}</span>
+        <input type="number" class="form-control" placeholder="${personal[i]}" aria-label="Username" aria-describedby="basic-addon1" disabled>
+      </div>
+    </div>
+    `;
+  }
+  personalCode = personalCode + `</br>`;
+  $('.summaryPersonal').html(personalCode);
+
+  // Address initial setup
+
+  let address = [ "Address", "Distrinct", "Province", "ZipCode", "Telephone" ];
+  let addressCode = '<p>Address</p>';
+  for(let i = 0 ; i < address.length ; i++) {
+    addressCode = addressCode + `
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon1">${address[i]}</span>
+        <input type="number" class="form-control" placeholder="${address[i]}" aria-label="Username" aria-describedby="basic-addon1" disabled>
+      </div>
+    </div>
+    `;
+  }
+  addressCode = addressCode + `</br>`;
+  $('.summaryAppointment').html(addressCode);
+
+  // Payment initial setup
+
+  let payment = [
+    {
+      topic: "Customer name",
+      detailForm: "Varit Assavavisidchai"
+    },
+    {
+      topic: "Reference no.",
+      detailForm: "1473857365977"
+    },
+    {
+      topic: "Date",
+      detailForm: "10-10-2010"
+    },
+    {
+      topic: "Time",
+      detailForm: "22.10"
+    },
+  ];
+  let paymentCode = '<p>Payment</p>';
+  for(let i = 0 ; i < payment.length ; i++) {
+    paymentCode = paymentCode + `
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon1">${payment[i].topic}</span>
+        <input type="number" class="form-control" placeholder="${payment[i].detailForm}" aria-label="Username" aria-describedby="basic-addon1" disabled>
+      </div>
+    </div>
+    `;
+  }
+  paymentCode = paymentCode + `</br>`;
+  $('.summaryPayment').html(paymentCode);
+
+}
