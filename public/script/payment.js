47d71ad51b83dd0f5e9b53061a21c9aab12b03b2
Sathira Kittisukmongkol <nong.jamie.jamie@gmail.com>
2018-11-21 11:26:57 +0700

    Add verify section in Payment page

--- public/script/payment.js
+++ public/script/payment.js
@@ -14,114 +14,17 @@ $( () => {
     window.location = "http://localhost:3000/myappointment/"+username;
   });
 
-  function paymentFileUpload(num) {
-    if(num == 0) {
-      $(".paymentFieldUploadSentence").html(`
-        <p>Step 3 : Verify your transaction.</p>
-        `);
-      $(".paymentFieldUpload").html(`
-        <p>No need to upload of fill anything.</p>
-        `);
-    }
-    else {
-      $(".paymentFieldUploadSentence").html(`
-        <p>Step 3 : Verify your transaction.</p>
-        `);
-      $(".paymentFieldUpload").html(`
-        <input type="file" style="margin: 5px; border: 1px solid black;"/>
-        <button type="button" class="btn btn-primary" style="margin: 5px;">Upload
-        </button>
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="basic-addon1">Name</span>
-          <input type="text" class="form-control" placeholder="Varit Assa" aria-label="Username" aria-describedby="basic-addon1" >
-        </div>
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="basic-addon1">Date</span>
-          <input type="text" class="form-control" placeholder="11 November 2018" aria-label="Username" aria-describedby="basic-addon1" >
-        </div>
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="basic-addon1">Time</span>
-          <input type="text" class="form-control" placeholder="22.53" aria-label="Username" aria-describedby="basic-addon1" >
-        </div>
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="basic-addon1">Amount(฿)</span>
-          <input type="text" class="form-control" placeholder="5,000" aria-label="Username" aria-describedby="basic-addon1" >
-        </div>
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="basic-addon1">Your acc. no.</span>
-          <input type="text" class="form-control" placeholder="123-4-56789-0" aria-label="Username" aria-describedby="basic-addon1" >
-        </div>
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="basic-addon1">Transaction number</span>
-          <input type="text" class="form-control" placeholder="340255905521903810" aria-label="Username" aria-describedby="basic-addon1" >
-        </div>
-        `);
-    }
-  };
+  // Pay with SCB
+  $(".scbWay").click( function action() {
+    payBySCB()
+  } );
 
-  $(".scbWay").click( () => {
-    $(".paymentFieldInputSentence").html(`
-      <p>Step 2 : Pay by the mobile banking to these info.</p>
-      `);
-    $(".paymentFieldInput").html(`
-      <div class="input-group mb-3">
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="basic-addon1">Bank</span>
-          <input type="number" class="form-control" placeholder="Siam Commercial Bank" aria-label="Username" aria-describedby="basic-addon1" disabled>
-        </div>
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="basic-addon1">Account's name</span>
-          <input type="text" class="form-control" placeholder="Varit Assavavisidchai" aria-label="Username" aria-describedby="basic-addon1" disabled>
-        </div>
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="basic-addon1">Account's type</span>
-          <input type="text" class="form-control" placeholder="Saving account" aria-label="Username" aria-describedby="basic-addon1" disabled>
-        </div>
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="basic-addon1">Branch</span>
-          <input type="text" class="form-control" placeholder="Bangkok" aria-label="Username" aria-describedby="basic-addon1" disabled>
-        </div>
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="basic-addon1">Acc. No.</span>
-          <input type="text" class="form-control" placeholder="591-0-54583-3" aria-label="Username" aria-describedby="basic-addon1" disabled>
-        </div>
-      </div>
-      `);
-      paymentFileUpload(1);
+  // Pay with KBank
+  $(".kbankWay").click( function action() {
+    payByKBank()
   });
 
-  $(".kbankWay").click( () => {
-    $(".paymentFieldInputSentence").html(`
-      <p>Step 2 : Pay by the mobile banking to these info.</p>
-      `);
-    $(".paymentFieldInput").html(`
-      <div class="input-group mb-3">
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="basic-addon1">Bank</span>
-          <input type="number" class="form-control" placeholder="Kasikorn Bank" aria-label="Username" aria-describedby="basic-addon1" disabled>
-        </div>
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="basic-addon1">Account's name</span>
-          <input type="text" class="form-control" placeholder="Varit Assavavisidchai" aria-label="Username" aria-describedby="basic-addon1" disabled>
-        </div>
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="basic-addon1">Account's type</span>
-          <input type="text" class="form-control" placeholder="Saving account" aria-label="Username" aria-describedby="basic-addon1" disabled>
-        </div>
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="basic-addon1">Branch</span>
-          <input type="text" class="form-control" placeholder="Bangkok" aria-label="Username" aria-describedby="basic-addon1" disabled>
-        </div>
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="basic-addon1">Acc. No.</span>
-          <input type="text" class="form-control" placeholder="591-0-54561-3" aria-label="Username" aria-describedby="basic-addon1" disabled>
-        </div>
-      </div>
-      `);
-      paymentFileUpload(1)
-  });
-
-  //fame part
+  // Fame part, Pay with Paypal
   paypal.Button.render({
     // Configure environment
     env: 'sandbox',
@@ -181,3 +84,82 @@ $( () => {
   }, '#paypal-button');
 
 } );
+
+function textInStep2() {
+  $(".paymentFieldInputSentence").html(`
+    <p>Step 2 : Pay by the mobile banking to these info.</p>
+  `);
+}
+
+function paymentFileUpload(num) {
+  let verifyExampleTopic = [ "Name", "Date", "Time", "Amount(฿)", "Acc. no.", "Transaction no." ];
+  let verifyExamplePlaceHolder = [ "Varit Assavavisidchai", "11 November 2018", "22.53", "5,000", "123-4-56789-0", "340255905521903810" ];
+  if(num == 0) {
+    $(".paymentFieldUploadSentence").html(`<p>Step 3 : Verify your transaction.</p>`);
+    $(".paymentFieldUpload").html(`<p>No need to upload of fill anything.</p>`);
+  }
+  else {
+    $(".paymentFieldUploadSentence").html(`
+      <p>Step 3 : Verify your transaction.</p>
+      <input type="file" style="font-size: 18px; margin: 5px; border: 1px solid black;"/>
+      <button type="button" class="btn btn-primary" style="margin: 5px;">Upload
+      </button>
+    `);
+    let ans = ``;
+    ans = ans + `
+      <div class="input-group mb-3">
+    `;
+    for(let i = 0 ; i < verifyExampleTopic.length ; i++) {
+      ans = ans + `
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="basic-addon1">${verifyExampleTopic[i]}</span>
+          <input type="text" class="form-control" placeholder=\"${verifyExamplePlaceHolder[i]}\" aria-label="Username" aria-describedby="basic-addon1" >
+        </div>
+      `;
+    }
+    ans = ans + `</div>`;
+    $(".paymentFieldUpload").html(`${ans}`);
+  }
+};
+
+function payBySCB() {
+  let payExampleTopic = [ "Bank", "Account's name", "Account's type", "Branch", "Acc. no." ];
+  let payExamplePlaceHolder = [ "Siam Commercial Bank", "Varit Assavavisidchai", "Saving account", "Bangkok", "591-0-54583-3" ];
+  textInStep2();
+  let ans = ``;
+  ans = ans + `
+    <div class="input-group mb-3">
+  `;
+  for(let i = 0 ; i < payExampleTopic.length ; i++) {
+    ans = ans + `
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon1">${payExampleTopic[i]}</span>
+        <input type="text" class="form-control" placeholder=\"${payExamplePlaceHolder[i]}\" aria-label="Username" aria-describedby="basic-addon1" disabled>
+      </div>
+    `;
+  }
+  ans = ans + `</div>`;
+  $(".paymentFieldInput").html(`${ans}`);
+  paymentFileUpload(1);
+}
+
+function payByKBank() {
+  let payExampleTopic = [ "Bank", "Account's name", "Account's type", "Branch", "Acc. no." ];
+  let payExamplePlaceHolder = [ "Kasikorn Bank", "Varit Assavavisidchai", "Saving account", "Bangkok", "891-0-11121-0" ];
+  textInStep2();
+  let ans = ``;
+  ans = ans + `
+    <div class="input-group mb-3">
+  `;
+  for(let i = 0 ; i < payExampleTopic.length ; i++) {
+    ans = ans + `
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon1">${payExampleTopic[i]}</span>
+        <input type="text" class="form-control" placeholder=\"${payExamplePlaceHolder[i]}\" aria-label="Username" aria-describedby="basic-addon1" disabled>
+      </div>
+    `;
+  }
+  ans = ans + `</div>`;
+  $(".paymentFieldInput").html(`${ans}`);
+  paymentFileUpload(1);
+}
