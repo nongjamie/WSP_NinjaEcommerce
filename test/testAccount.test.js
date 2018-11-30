a9ef8985196c14995b6d95f3f3d907ecd9250d4e
Totsapon Menkul <totsapon.m@ku.th>
2018-11-30 22:22:35 +0700

    test gitlab

--- test/testAccount.test.js
+++ test/testAccount.test.js
@@ -1,49 +1,49 @@
-const Account = require("../class/account")
+const Account = require('../class/account')
 const account = new Account()
 
-test("(Class: Account)Test get account list", async () => {
+test('(Class: Account)Test get account list', async () => {
   const result = await account.getAccountList()
-  expect(result.return_code).toMatch("200")
+  expect(result.return_code).toMatch('200')
 })
 
-test("(Class: Account)Test add account to database", async () => {
+test('(Class: Account)Test add account to database', async () => {
   const data = {
-    username: "TestAccount",
-    email: "varitisaboy11511@gmail.com",
-    password: "1234",
-    confirmPassword: "1234",
-    name: "totsapon menkul",
-    gender: "male",
-    day: "1",
-    month: "January",
-    year: "1951",
-    address: "bangkok thailand",
-    province: "Bangkok",
-    district: "",
-    zipCode: "11000",
-    telephone: "21"
+    username: 'TestAccount1121150',
+    email: 'varitisaboy11511@gmail.com',
+    password: '1234',
+    confirmPassword: '1234',
+    name: 'totsapon menkul',
+    gender: 'male',
+    day: '1',
+    month: 'January',
+    year: '1951',
+    address: 'bangkok thailand',
+    province: 'Bangkok',
+    district: '',
+    zipCode: '11000',
+    telephone: '21'
   }
   const result = await account.add(data)
-  expect(result.return_code).toMatch("200")
+  expect(result.return_code).toMatch('200')
 }, 30000)
 
-test("(Class: Account)Test login", async () => {
+test('(Class: Account)Test login', async () => {
   const data = {
-    username: "TestAccount",
-    password: "1234"
+    username: 'TestAccount1121150',
+    password: '1234'
   }
   const result = await account.login(data)
-  expect(result.return_code).toMatch("200")
+  expect(result.return_code).toMatch('200')
 }, 30000)
 
-test("(Class: Account)Test get account by username", async () => {
-  const username = "TestAccount"
+test('(Class: Account)Test get account by username', async () => {
+  const username = 'TestAccount1121150'
   const result = await account.getAccountBy(username)
-  expect(result.return_code).toMatch("200")
+  expect(result.return_code).toMatch('200')
 }, 30000)
 
-test("(Class: Account)Test remove account", async () => {
-  const username = "TestAccount"
+test('(Class: Account)Test remove account', async () => {
+  const username = 'TestAccount1121150'
   const result = await account.remove(username)
-  expect(result.return_code).toMatch("200")
+  expect(result.return_code).toMatch('200')
 }, 30000)
