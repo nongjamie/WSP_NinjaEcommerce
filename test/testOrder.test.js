5f6118ac64e56eb36a59c02f74596cfe762f1dfb
Totsapon Menkul <totsapon.m@ku.th>
2018-12-02 01:23:43 +0700

    update test

--- test/testOrder.test.js
+++ test/testOrder.test.js
@@ -1,7 +1,12 @@
 const Order = require('../class/order')
 const order = new Order()
 
-test("(Class: Order)Test get order list", async () => {
-    const result = await order.getOrderList()
-    expect(result.return_code).toMatch("200")
-  }, 30000)
+test('(Class: Order)Test get order list', async () => {
+  const result = await order.getOrderList()
+  expect(result.return_code).toMatch('200')
+}, 30000)
+
+test('(Class: Order)Test get order by ID', async () => {
+  const result = await order.getOrderByOrderID('hereton')
+  expect(result.return_code).toMatch('200')
+}, 30000)
