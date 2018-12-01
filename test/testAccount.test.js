7fd2121e3c80a7ad3590bbbc2b0ef19dde929e44
Totsapon Menkul <totsapon.m@ku.th>
2018-12-02 02:25:30 +0700

    update test

--- test/testAccount.test.js
+++ test/testAccount.test.js
@@ -3,12 +3,13 @@ const account = new Account()
 
 test('(Class: Account)Test get account list', async () => {
   const result = await account.getAccountList()
+  console.log(result)
   expect(result.return_code).toMatch('200')
 })
 
 test('(Class: Account)Test add account to database', async () => {
   const data = {
-    username: 'TestAccount1121150',
+    username: 'TestAccount1129Q810',
     email: 'varitisaboy11511@gmail.com',
     password: '1234',
     confirmPassword: '1234',
@@ -24,27 +25,31 @@ test('(Class: Account)Test add account to database', async () => {
     telephone: '21'
   }
   const result = await account.add(data)
+  console.log(result)
   expect(result.return_code).toMatch('200')
 }, 30000)
 
 test('(Class: Account)Test login', async () => {
   const data = {
-    username: 'TestAccount1121150',
+    username: 'TestAccount1129Q810',
     password: '1234'
   }
   const result = await account.login(data)
+  console.log(result)
   expect(result.return_code).toMatch('200')
 }, 30000)
 
 test('(Class: Account)Test get account by username', async () => {
-  const username = 'TestAccount1121150'
+  const username = 'TestAccount1129Q810'
   const result = await account.getAccountBy(username)
+  console.log(result)
   expect(result.return_code).toMatch('200')
 }, 30000)
 
 test('(Class: Account)Test remove account', async () => {
-  const username = 'TestAccount1121150'
+  const username = 'TestAccount1129Q810'
   const result = await account.remove(username)
+  console.log(result)
   expect(result.return_code).toMatch('200')
 }, 30000)
 
@@ -54,5 +59,6 @@ test('(Class: Account)Test Admin login', async () => {
     password: 'ninjavarit'
   }
   const result = await account.loginToAdmin(data)
+  console.log(result)
   expect(result.return_code).toMatch('200')
 }, 30000)
