f3484312232bf7e6df5feb21b8a09714f4f7386c
Totsapon Menkul <totsapon.m@ku.th>
2018-12-01 19:09:51 +0700

    update test

--- test/testCart.test.js
+++ test/testCart.test.js
@@ -1,44 +1,44 @@
-const Cart = require("../class/cart")
+const Cart = require('../class/cart')
 const cart = new Cart()
-const Product = require("../class/product")
+const Product = require('../class/product')
 const product = new Product()
 
-test("(Class: Cart) Add product to cart", async () => {
+test('(Class: Cart) Add product to cart', async () => {
   const data = {
-    username: "hereton",
-    productID: "4JbTehkukn42UPXr0H9c",
+    username: 'hereton',
+    productID: '4JbTehkukn42UPXr0H9c',
     quantity: 2
   }
   const result = await product.addProductToCart(data)
-  expect(result.return_code).toMatch("200")
-})
+  expect(result.return_code).toMatch('200')
+}, 30000)
 
-test("(Class: Cart)Test get user cart", async () => {
+test('(Class: Cart)Test get user cart', async () => {
   const data = {
-    username: "hereton"
+    username: 'hereton'
   }
   const result = await cart.getUserCart(data)
-  expect(result.return_code).toMatch("200")
-})
+  expect(result.return_code).toMatch('200')
+}, 30000)
 
-test("(Class: Cart)Test remove product from cart", async () => {
+test('(Class: Cart)Test remove product from cart', async () => {
   const data = {
-    username: "hereton",
-    productID: "4JbTehkukn42UPXr0H9c",
+    username: 'hereton',
+    productID: '4JbTehkukn42UPXr0H9c',
     quantity: 1
   }
   const result = await cart.removeProductFromCart(data)
-  expect(result.return_code).toMatch("200")
-})
+  expect(result.return_code).toMatch('200')
+}, 30000)
 
-test("(Class: Cart)Test check out", async () => {
+test('(Class: Cart)Test check out', async () => {
   const data1 = {
-    username: "hereton",
-    productID: "4JbTehkukn42UPXr0H9c",
+    username: 'hereton',
+    productID: '4JbTehkukn42UPXr0H9c',
     quantity: 1
   }
-  const result = await cart.checkoutByUsername("hereton")
+  const result = await cart.checkoutByUsername('hereton')
   console.log(result)
   expect(result.orderID)
-  expect(result.return_code).toMatch("200")
+  expect(result.return_code).toMatch('200')
 }, 30000)
--- test/testFeedback.test.js
+++ test/testFeedback.test.js
@@ -1,19 +1,19 @@
-const Feedback = require("../class/feedback")
+const Feedback = require('../class/feedback')
 const feedback = new Feedback()
 
-test("(Class: Feedback) Add and remove feedback", async () => {
+test('(Class: Feedback) Add and remove feedback', async () => {
   const data = {
-    name: "Taksin",
-    feedback: "I need to buy tanks"
+    name: 'Taksin',
+    feedback: 'I need to buy tanks'
   }
   const result = await feedback.addFeedback(data)
-  expect(result.return_code).toMatch("200")
+  expect(result.return_code).toMatch('200')
   const feedbackID = result.feedbackID
   const removeResult = await feedback.removeFeedback(feedbackID)
-  expect(removeResult.return_code).toMatch("200")
+  expect(removeResult.return_code).toMatch('200')
 }, 30000)
 
-test("(Class: Feedback) Get feedbacks", async () => {
+test('(Class: Feedback) Get feedbacks', async () => {
   const result = await feedback.getFeedbacks()
-  expect(result.return_code).toMatch("200")
+  expect(result.return_code).toMatch('200')
 }, 30000)
